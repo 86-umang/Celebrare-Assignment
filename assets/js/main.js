b7999523c3396c7c8805fd828750eab446a8364d
Umangkumar Patel <umangkumar.18beceg031@gmail.com>
2022-05-30 16:20:52 +0530

    Final_V0.0.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,27 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
     navbarLinks1.classList.toggle('active')
 })
+
+
+
+const modalViews = document.querySelectorAll('.modal'),
+      modalBtns = document.querySelectorAll('.modal_button'),
+      modalCloses = document.querySelectorAll('.modal_close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
